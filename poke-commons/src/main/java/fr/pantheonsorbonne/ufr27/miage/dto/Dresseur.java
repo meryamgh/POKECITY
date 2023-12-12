0ceeec6c4496d00f93e51ccf3be9ff09a481af91
Pauline <pauline.auda@free.fr>
2023-12-12 16:41:21 +0100

    début get acheter pokemon

--- poke-commons/src/main/java/fr/pantheonsorbonne/ufr27/miage/dto/Dresseur.java
+++
@@ -1,6 +0,0 @@
-package fr.pantheonsorbonne.ufr27.miage.dto;
-
-import java.util.List;
-
-public record Dresseur(Integer idDresseur, String name, boolean bannedStatus, List<Pokemon> pokedex) {
-}
--- poke-commons/src/main/java/fr/pantheonsorbonne/ufr27/miage/dto/Pokemon.java
+++
@@ -1,3 +0,0 @@
-package fr.pantheonsorbonne.ufr27.miage.dto;
-
-public record Pokemon(Integer idPokemon, String type, Integer pokeScore, Boolean isAdopted) {}
